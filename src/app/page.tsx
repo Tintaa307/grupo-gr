2ea723d5022425fbfff2d74295ae584644013072
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2025-05-21 18:16:37 -0300

    Comment out CertificacionBanner in page.tsx to streamline layout and enhance visual clarity.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
       {/* <LegislaturaBanner /> */}
       <Features />
       <RecentWorks />
-      <CertificacionBanner />
+      {/* <CertificacionBanner /> */}
       <Services />
       <InstructivoBanner />
       <CTASection />
