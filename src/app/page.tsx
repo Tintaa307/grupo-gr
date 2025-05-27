50524340c001621797f3fe800fc20fd64497a6a0
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2025-05-27 17:25:46 -0300

    Update Home component layout to adjust carousel height for mobile responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,7 +92,7 @@ export default function Home() {
             </Link>
           </div>
 
-          <div className="relative h-[500px] w-full">
+          <div className="relative h-[500px] w-full max-md:h-[400px]">
             <Carousel images={carouselImages} />
           </div>
         </div>
