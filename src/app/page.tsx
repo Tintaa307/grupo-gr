6693aa677a09c2230b924903f04454fc74d34437
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2025-05-27 17:30:41 -0300

    Update Home component layout to add bottom margin to carousel for improved spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,7 +92,7 @@ export default function Home() {
             </Link>
           </div>
 
-          <div className="relative h-[500px] w-full max-md:h-[400px]">
+          <div className="relative h-[500px] w-full max-md:h-[400px] mb-12">
             <Carousel images={carouselImages} />
           </div>
         </div>
